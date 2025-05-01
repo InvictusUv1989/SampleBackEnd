3540d96f623b81c3d9e617e8eaefcf2263025596
InvictusUv1989 <mail.uvraj@gmail.com>
2025-04-30 20:44:17 -0500

    Refacting of Product Module

--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -1,33 +1,26 @@
 ﻿using Newtonsoft.Json;
 using SampleBackEnd.Model;
+using SampleBackEnd.Utitlites;
 
 namespace SampleBackEnd.Services
 {
     public class ProductService
     {
-
+        const string filename = "Data/product.json";
         public static List<Product> GetProductData()
         {
-            using (StreamReader r = new StreamReader("Data/product.json"))
-            {
-                string json = r.ReadToEnd();
-                List<Product> items = JsonConvert.DeserializeObject<List<Product>>(json);
-
-                return items;
-            }
+            return JsonToObject(filename);
 
         }
 
         public static Product GetProductByCode(string productCode)
         {
-            using (StreamReader r = new StreamReader("Data/product.json"))
-            {
-                string json = r.ReadToEnd();
-                List<Product> items = JsonConvert.DeserializeObject<List<Product>>(json);
+            return JsonToObject(filename).FirstOrDefault(x => x.ProductCode == productCode);
+        }
 
-                return items.FirstOrDefault(x => x.ProductCode == productCode);
-            }
-
+        private static List<Product> JsonToObject(string filename)
+        {
+            return JsonUtility.JsonToObject<List<Product>>(filename);
         }
     }
 }
---
+++ Utitlites/JsonUtility.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace SampleBackEnd.Utitlites
+{
+    public class JsonUtility
+    {
+        public static T JsonToObject<T>(string filename) {
+
+            using (StreamReader r = new StreamReader(filename))
+            {
+                string json = r.ReadToEnd();
+                return JsonConvert.DeserializeObject<T>(json);
+
+            }
+
+        }
+    }
+}
